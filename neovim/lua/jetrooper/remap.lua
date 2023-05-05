1ff05e8ea586095ed3c7d4af92ed894fde02d29d
Jethro Ordaneza <jethro@poplarhomes.com>
2023-05-05 21:07:43 +0800

    Cleanup mappings
    * Removed unused mappings
    * Added descriptive comments to mappings

--- neovim/lua/jetrooper/remap.lua
+++ neovim/lua/jetrooper/remap.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Sticky visual move
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -10,24 +11,15 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
-end)
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
-end)
-
--- greatest remap ever
+-- Always paste the same word
 vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("v", "p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
+-- Copy/Yank to system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
-
--- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
@@ -38,4 +30,9 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+-- Replace all
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Resize window
+vim.keymap.set("n", "<leader>h", ":vertical resize -2<CR>")
+vim.keymap.set("n", "<leader>l", ":vertical resize +2<CR>")
