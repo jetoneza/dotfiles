eff55450c534eefb7fa0424073328128c44e8705
Jethro Ordaneza <jethro@poplarhomes.com>
2023-05-08 18:30:45 +0800

    Reuse formatter for js and ts

--- neovim/after/plugin/formatter.lua
+++ neovim/after/plugin/formatter.lua
@@ -1,6 +1,17 @@
 -- Utilities for creating configurations
 local util = require "formatter.util"
 
+-- prettierd formatter
+prettierd = {
+  function()
+    return {
+      exe = "prettierd",
+      args = {vim.api.nvim_buf_get_name(0)},
+      stdin = true
+    }
+  end
+}
+
 -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
 require("formatter").setup {
   -- Enable or disable logging
@@ -9,26 +20,8 @@ require("formatter").setup {
   log_level = vim.log.levels.WARN,
   -- All formatter configurations are opt-in
   filetype = {
-    javascript = {
-        -- prettierd
-       function()
-          return {
-            exe = "prettierd",
-            args = {vim.api.nvim_buf_get_name(0)},
-            stdin = true
-          }
-        end
-    },
-    typescript = {
-        -- prettierd
-       function()
-          return {
-            exe = "prettierd",
-            args = {vim.api.nvim_buf_get_name(0)},
-            stdin = true
-          }
-        end
-    },
+    javascript = prettierd,
+    typescript = prettierd,
     -- Use the special "*" filetype for defining formatter configurations on
     -- any filetype
     ["*"] = {
