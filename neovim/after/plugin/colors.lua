c9b3b3525a6e30f25fa791427d6516b5e183acd6
Jethro Ordaneza <jet.oneza@gmail.com>
2023-06-05 12:00:40 +0800

    Add background transparency to nvim

--- neovim/after/plugin/colors.lua
+++ neovim/after/plugin/colors.lua
@@ -1,6 +1,10 @@
 function ApplyTheme(color)
     color = color or "catppuccin-macchiato"
     vim.cmd.colorscheme(color)
+
+    -- Uncomment to make bg transparent
+    -- vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+    -- vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
 end
 
 ApplyTheme()
