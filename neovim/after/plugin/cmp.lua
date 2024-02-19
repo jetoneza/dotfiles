eb2d1343189bfea8931578920e23280ecf417928
Jethro Ordaneza <jet.oneza@gmail.com>
2024-02-19 15:46:19 +0800

    chore: add completion hierarchy

--- neovim/after/plugin/cmp.lua
+++ neovim/after/plugin/cmp.lua
@@ -3,5 +3,8 @@ local cmp = require('cmp')
 cmp.setup {
   sources = {
     { name = "copilot", group_index = 2, priority = 100 },
+    { name = "nvim_lsp", group_index = 2 },
+    { name = "path", group_index = 2 },
+    { name = "luasnip", group_index = 2 },
   },
 }
