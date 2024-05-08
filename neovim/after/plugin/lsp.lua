9af3fa4b01cceb2cb9695f70c5f7442e72a546e6
Jethro Ordaneza <jet.oneza@gmail.com>
2024-05-08 09:06:08 +0800

    chore: remove unused variables in lsp config

--- neovim/after/plugin/lsp.lua
+++ neovim/after/plugin/lsp.lua
@@ -10,7 +10,6 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<cr>'] = cmp.mapping.confirm({select = true}),
     ["<C-Space>"] = cmp.mapping.complete()
@@ -18,7 +17,7 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 
 lsp.setup_nvim_cmp({mapping = cmp_mappings})
 
-lsp.on_attach(function(client, bufnr)
+lsp.on_attach(function(_, bufnr)
     local opts = {buffer = bufnr, remap = false}
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
