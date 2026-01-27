11a18a3be6e9446c2c17b088ce16d9368f94d892
Jet Ordaneza <jethro@bookipi.com>
2026-01-27 14:58:59 +0800

    chore: update lsp config

--- neovim/after/plugin/lsp.lua
+++ neovim/after/plugin/lsp.lua
@@ -1,61 +1,62 @@
-local neodev = require('neodev')
-
--- IMPORTANT: make sure to setup neodev BEFORE lspconfig
-neodev.setup({
-    -- add any options here, or leave empty to use the default settings
-})
-
-local lsp = require("lsp-zero")
-
-lsp.preset("recommended")
+require('mason').setup()
+require('mason-lspconfig').setup()
 
 local cmp = require('cmp')
-local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<cr>'] = cmp.mapping.confirm({select = true}),
-    ["<C-Space>"] = cmp.mapping.complete()
+cmp.setup({
+    mapping = cmp.mapping.preset.insert({
+        ["<CR>"] = cmp.mapping.confirm({select = true}),
+        ["<C-Space>"] = cmp.mapping.complete(),
+
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end, {"i", "s"}),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end, {"i", "s"})
+    }),
+    sources = cmp.config.sources({{name = "nvim_lsp"}, {name = "buffer"}})
 })
 
-lsp.setup_nvim_cmp({mapping = cmp_mappings})
+local lsp_capabilities = vim.tbl_deep_extend(
+  "force",
+  vim.lsp.protocol
+  .make_client_capabilities(),
+  require("cmp_nvim_lsp").default_capabilities()
+)
 
-lsp.on_attach(function(_, bufnr)
+local lsp_attach = function(_, bufnr)
     local opts = {buffer = bufnr, remap = false}
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vws",
-                   function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd",
-                   function() vim.diagnostic.open_float() end, opts)
+
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end,
-                   opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end,
-                   opts)
-    vim.keymap
-        .set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end,
                    opts)
-end)
+end
 
-lsp.setup()
+vim.lsp.config('*', {on_attach = lsp_attach, capabilities = lsp_capabilities})
 
-require("mason-lspconfig").setup {
-    handlers = {
-        lua_ls = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup {
-                settings = {
-                    Lua = {
-                        -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-                        diagnostics = { disable = { 'missing-fields' } },
-                    }
-                }
-            }
-
-            lspconfig.jdtls.setup{}
-        end
-    }
-}
-
-vim.diagnostic.config({virtual_text = true})
+vim.diagnostic.config({
+  virtual_text = true,
+  severity_sort = true,
+  float = { border = "rounded" },
+})
